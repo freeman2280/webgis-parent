8d18b9a3fc77e069ad502acc6c342fa976fd6399
crondacloud <2280781256@qq.com>
2023-08-02 11:37:11 +0800

    second commit test 开发环境的搭建 陈锐

--- webgis-raster/src/main/java/com/henu/controller/TestController.java
+++ webgis-raster/src/main/java/com/henu/controller/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/test")
     String test() {
-        return "Raster ok";
+        return "raster ok";
     }
 }
